0ab90a74f56ba5f5191505de065088211e32a127
Sasikumar111 <112794204+Sasikumar111@users.noreply.github.com>
2023-06-29 12:34:04 +0530

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
   const handleApiCall = async (e) => {
     // const url = 'http://localhost:9999/'+e.target.name;
-    let url = 'http://65.2.186.100:32470/'+e.target.name
+    let url = 'http://43.205.178.173:32470/'+e.target.name
 
     displayData.url=url
     let options = {}
